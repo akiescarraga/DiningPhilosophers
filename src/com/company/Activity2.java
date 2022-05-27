b61a3e22fd09dd96e9659d86c8239c0445eaab72
alennacayton <cayton.alenna@gmail.com>
2022-05-27 22:44:55 +0800

    added driver class

--- src/com/company/Activity2.java
+++ src/com/company/Activity2.java
@@ -3,6 +3,21 @@ package com.company;
 public class Activity2 {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Chopstick[] chopstick = new Chopstick[5];
+        Philosopher[] philospher = new Philosopher[5];
+        State hlp = new State();
+
+        for (int i = 0; i < 5; i++) {
+
+            chopstick[i] = new Chopstick();
+
+        }
+        for (int i = 0; i < 5; i++) {
+            philospher[i] = new Philosopher(hlp, chopstick[i], chopstick[(i + 4) % 5],i);
+            new Thread(philospher[i]).start();
+
+        }
+
     }
 }
